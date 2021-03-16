29c6dbedcb6f38e022d2f7c94ad6ef37844811e6
DevGenix <you@example.com>
2021-03-16 13:23:59 -0400

    Added email functionality

--- Auth/welcomer.py
+++ Auth/welcomer.py
@@ -2,8 +2,23 @@ import random
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import smtplib
 
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
-os.getenv("SECURITY_CODE") = random.randint(100, 10000) 
+security_code = random.randint(100, 100000)
+
+def send_email():
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(email, password)
+
+    subject = "Your verification code to login"
+    body = "Hey this your verification code {}".format(security_code)
+    message = "Subject: {}".format(subject) + " \n\n {}".format(body)
+    server.sendmail(email,email, message)
+    print("EMAIL HAS BEEN SENT!")
+send_email()
 
 
