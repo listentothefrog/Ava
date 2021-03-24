8fc9305b3c19fb290d5b610a00ab004ea8026e78
DevGenix <you@example.com>
2021-03-24 14:22:24 -0400

    Creates a playlist

--- Music/recommend.py
+++ Music/recommend.py
@@ -1,16 +1,38 @@
 import json
-import os 
-
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-
+import requests
+import datetime
+from secrets import user_id, bearer
 class RecommendMusic:
   def __init__(self):
-      pass
-  def recommend_music(self):
-    print("Music....")
+    self.user_id = user_id
+    self.bearer = bearer
+  
+  def create_playlist(self):
+        today_date = datetime.date.today()
+        body = json.dumps({
+          "name": f"{today_date} Playlist",
+          "description": "A playlist with song recommends for Shashank to listen to",
+            "public": False
+        })
+        query = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+        res = requests.post(
+          query,
+          data=body,
+          headers ={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {bearer}"
+          }
+        )
+        response = res.json()
+        print(response)
     
+  def recommend_music(self):
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 21 and hour <= 22:
+          print("I have created a playlist with song recommends for Shashank to listen to")
+          rc.create_playlist()
+    else:
+          print("Not the time to do it...")
 
 rc = RecommendMusic()
 rc.recommend_music()
