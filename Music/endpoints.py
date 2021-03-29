3e3ff78975e6b197bb1eb9f8e6acb44bb7811c00
DevGenix <you@example.com>
2021-03-28 20:51:49 -0400

    Endpoints for spotify

---
+++ Music/endpoints.py
@@ -0,0 +1,8 @@
+end_point_url = "https://api.spotify.com/v1/recommendations"
+
+# filters
+limit=10
+market="US"
+seed_artists="3zw9kN0i8eKIDbGhEiIIwo"
+seed_genres="lofi"
+seed_tracks="0vvXsWCC9xrXsKd4FyS8kM"
