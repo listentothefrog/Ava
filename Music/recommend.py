27fbf1b84c32f1eeebc6ea555dd87f5f2786e8e0
DevGenix <you@example.com>
2021-03-28 20:50:59 -0400

    Searches for music based on seeds

--- Music/recommend.py
+++ Music/recommend.py
@@ -1,40 +1,34 @@
-import json
-import requests
 import datetime
-from secrets import user_id, bearer
+from secrets import bearer
 import time
+import requests
+from endpoints import end_point_url, limit, market,  seed_artists, seed_genres, seed_tracks
+from pprint import pprint
 class RecommendMusic:
   def __init__(self):
-    self.user_id = user_id
+    self.end_point_url = end_point_url
+    self.limit = limit
+    self.market = market
+    self.seed_artists = seed_artists
+    self.seed_genres = seed_genres
+    self.seed_tracks = seed_tracks
     self.bearer = bearer
-  
-  def create_playlist(self):
-        today_date = datetime.date.today()
-        body = json.dumps({
-          "name": f"{today_date} Playlist",
-          "description": "A playlist with song recommends for Shashank to listen to",
-            "public": False
-        })
-        query = f"https://api.spotify.com/v1/users/{self.user_id}/playlists"
-        res = requests.post(
-          query,
-          data=body,
-          headers ={
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.bearer}"
-          }
-        )
-        response = res.json()
-        print(response)
     
-  def recommend_music(self):
+  def get_recommended_music(self):
+      query = f"{self.end_point_url}?limit={self.limit}&market={self.market}&seed_artists={self.seed_artists}&seed_genres={self.seed_genres}&seed_tracks={self.seed_tracks}"
+      response =requests.get(query, 
+               headers={"Content-Type":"application/json", 
+                        "Authorization":f"Bearer {self.bearer}"})
+      res = response.json()
+      pprint(res) 
+  def time_check(self):
     hour = int(datetime.datetime.now().hour)
     if hour >= 21 and hour <= 22:
           print("I have created a playlist with song recommends for Shashank to listen to")
-          rc.create_playlist()
+          rc.get_recommended_music()
     else:
           print("Not the time to do it...")
     time.sleep(2700)
-
+                
 rc = RecommendMusic()
-rc.recommend_music()
+rc.time_check()
