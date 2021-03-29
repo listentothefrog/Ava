3cd4785681658f49a22e7c7e97dd86f132140c49
DevGenix <you@example.com>
2021-03-29 09:05:56 -0400

    adds recommended songs to playlist

--- Music/recommend.py
+++ Music/recommend.py
@@ -1,8 +1,9 @@
 import datetime
-from secrets import bearer
+from secrets import bearer, user_id
 import time
+import json
 import requests
-from endpoints import end_point_url, limit, market,  seed_artists, seed_genres, seed_tracks
+from endpoints import end_point_url, limit, market,  seed_artists, seed_genres, seed_tracks, uris
 
 class RecommendMusic:
   def __init__(self):
@@ -13,7 +14,7 @@ class RecommendMusic:
     self.seed_genres = seed_genres
     self.seed_tracks = seed_tracks
     self.bearer = bearer
-    
+    self.user_id = user_id
   def get_recommended_music(self):
       query = f"{self.end_point_url}?limit={self.limit}&market={self.market}&seed_artists={self.seed_artists}&seed_genres={self.seed_genres}&seed_tracks={self.seed_tracks}"
       response =requests.get(query, 
@@ -21,15 +22,41 @@ class RecommendMusic:
                         "Authorization":f"Bearer {self.bearer}"})
       res = response.json()
       for i in res['tracks']:
-            print(f"{i['name']} by {i['artists'][0]['name']}") 
+            uris.append(f"{i['uri']}")
+  def create_playlist(self):
+      today_date = datetime.date.today()
+      body = json.dumps({
+          "name": f"{today_date} Playlist",
+          "description": "A playlist with song recommends for Shashank to listen to",
+            "public": False
+        })
+      query = f"https://api.spotify.com/v1/users/{self.user_id}/playlists"
+      res = requests.post(
+          query,
+          data=body,
+          headers ={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.bearer}"
+          }
+        )  
+      playlist_id = res.json()['id']
+      playlist_url = res.json()['external_urls']['spotify']
+      endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+      request_body = json.dumps({
+          "uris" : uris
+        })
+      response = requests.post(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
+                      "Authorization":f"Bearer {self.bearer}"})
+      print(f'Your playlist is ready at {playlist_url}')
+  
   def time_check(self):
     hour = int(datetime.datetime.now().hour)
     if hour >= 7 and hour <= 8:
-          print("I have created a playlist with song recommends for Shashank to listen to")
           rc.get_recommended_music()
+          rc.create_playlist()
     else:
           print("Not the time to do it...")
     time.sleep(2700)
-                
+
 rc = RecommendMusic()
 rc.time_check()
