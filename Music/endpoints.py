06e4bf1f488cc87113df8e491683dbafb924f330
DevGenix <you@example.com>
2021-03-29 09:05:14 -0400

    added uri array so that ava can add songs to my playlist

--- Music/endpoints.py
+++ Music/endpoints.py
@@ -3,6 +3,7 @@ end_point_url = "https://api.spotify.com/v1/recommendations"
 # filters
 limit=10
 market="US"
+uris = []
 seed_artists="3zw9kN0i8eKIDbGhEiIIwo"
 seed_genres="lofi"
 seed_tracks="0vvXsWCC9xrXsKd4FyS8kM"
